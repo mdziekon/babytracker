dae6c3745b37933c0232412f8e5db58d9806f7e4
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2025-01-13 03:31:13 +0100

    feat: Allow to remove entry from edit view
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>

--- src/features/EventEditor/CompleteEvent/CompleteEvent.tsx
+++ src/features/EventEditor/CompleteEvent/CompleteEvent.tsx
@@ -1,6 +1,11 @@
+import { Button, Text } from '@mantine/core';
 import { LogEntry } from '../../../common/store/store.types';
 import { EventCard } from '../EventCard/EventCard';
 import { EventDetails } from '../EventDetails/EventDetails';
+import { useDisclosure } from '@mantine/hooks';
+import { EntryDeleteModal } from '../../../common/features/EntryDeleteModal/EntryDeleteModal';
+import { useNavigate } from 'react-router';
+import { IconTrash } from '@tabler/icons-react';
 
 interface CompleteEventProps {
     event: LogEntry;
@@ -8,11 +13,44 @@ interface CompleteEventProps {
 
 export const CompleteEvent = (props: CompleteEventProps) => {
     const { event } = props;
+    const navigate = useNavigate();
+
+    const [
+        isConfirmDeleteOpen,
+        { open: openConfirmDelete, close: closeConfirmDelete },
+    ] = useDisclosure(false);
 
     return (
-        <EventCard
-            eventType={event.entryType}
-            middle={<EventDetails event={event} />}
-        />
+        <>
+            <EventCard
+                eventType={event.entryType}
+                middle={<EventDetails event={event} />}
+                footer={
+                    <>
+                        <Button
+                            variant="light"
+                            color="red"
+                            fullWidth
+                            onClick={openConfirmDelete}
+                        >
+                            <IconTrash />
+                            <Text component="span" ml="0.25rem">
+                                Delete entry
+                            </Text>
+                        </Button>
+                    </>
+                }
+            />
+
+            <EntryDeleteModal
+                entry={event}
+                isModalOpen={isConfirmDeleteOpen}
+                onModalClose={closeConfirmDelete}
+                onEntryDeleted={() => {
+                    // TODO: This interaction causes error due to EventEditor missing id
+                    void navigate('/log');
+                }}
+            />
+        </>
     );
 };
