b23ad74e4224e7db35be1c085d37bc64a569690a
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2025-01-18 01:51:37 +0100

    feat: Allow to show event icon badges

--- src/features/EventEditor/EventCard/EventCard.tsx
+++ src/features/EventEditor/EventCard/EventCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Card, Text } from '@mantine/core';
+import { Avatar, Badge, Box, Card, Center, Text } from '@mantine/core';
 import { EntryType } from '../../../common/store/store.types';
 import {
     mapEntryTypeToColor,
@@ -11,25 +11,76 @@ import { Children } from 'react';
 
 interface EventCardProps {
     eventType: EntryType;
+    eventIconBadges?: {
+        ['top-left']?: React.JSX.Element;
+        ['top-right']?: React.JSX.Element;
+        ['bottom-left']?: React.JSX.Element;
+        ['bottom-right']?: React.JSX.Element;
+    };
     middle?: React.ReactNode;
     footer?: React.ReactNode;
 }
 
 export const EventCard = (props: EventCardProps) => {
-    const { eventType, middle, footer } = props;
+    const { eventType, eventIconBadges, middle, footer } = props;
     const EntryTypeIcon = mapEntryTypeToIcon(eventType);
 
     return (
         <Card withBorder padding="lg" radius="md" className={classes.card}>
             <Card.Section px="xs" py="lg">
-                <Avatar
-                    color={mapEntryTypeToColor(eventType)}
-                    size={120}
-                    radius={120}
-                    mx="auto"
-                >
-                    <EntryTypeIcon style={{ width: '70%', height: '70%' }} />
-                </Avatar>
+                <Center>
+                    <Box className={classes.avatarBox}>
+                        <Avatar
+                            color={mapEntryTypeToColor(eventType)}
+                            size={120}
+                            radius={120}
+                            mx="auto"
+                        >
+                            <EntryTypeIcon
+                                style={{ width: '70%', height: '70%' }}
+                            />
+                        </Avatar>
+                        {eventIconBadges?.['top-left'] && (
+                            <Badge
+                                {...eventIconBadges['top-left'].props}
+                                className={[
+                                    classes.avatarBadgeTopLeft,
+                                    eventIconBadges['top-left'].props.className,
+                                ].join(' ')}
+                            />
+                        )}
+                        {eventIconBadges?.['top-right'] && (
+                            <Badge
+                                {...eventIconBadges['top-right'].props}
+                                className={[
+                                    classes.avatarBadgeTopRight,
+                                    eventIconBadges['top-right'].props
+                                        .className,
+                                ].join(' ')}
+                            />
+                        )}
+                        {eventIconBadges?.['bottom-left'] && (
+                            <Badge
+                                {...eventIconBadges['bottom-left'].props}
+                                className={[
+                                    classes.avatarBadgeBottomLeft,
+                                    eventIconBadges['bottom-left'].props
+                                        .className,
+                                ].join(' ')}
+                            />
+                        )}
+                        {eventIconBadges?.['bottom-right'] && (
+                            <Badge
+                                {...eventIconBadges['bottom-right'].props}
+                                className={[
+                                    classes.avatarBadgeBottomRight,
+                                    eventIconBadges['bottom-right'].props
+                                        .className,
+                                ].join(' ')}
+                            />
+                        )}
+                    </Box>
+                </Center>
             </Card.Section>
 
             <Text fw={700} className={classes.title} mt="xs">
