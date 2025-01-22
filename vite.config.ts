0d30c37fbfd080b2e27d7128f36494cf055899e1
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2025-01-22 23:54:44 +0100

    feat: Add version commit to settings
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import * as child from 'child_process';
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const commitHash = child
+    .execSync('git rev-parse --short HEAD')
+    .toString()
+    .trim();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -47,5 +53,8 @@ export default defineConfig({
             '@tabler/icons-react':
                 '@tabler/icons-react/dist/esm/icons/index.mjs',
         },
+    },
+    define: {
+        'import.meta.env.VITE_APP_COMMIT': JSON.stringify(commitHash),
     },
 });
