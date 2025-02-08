0dc06d4157fc328b17c8ae331d5479c90412fe72
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2025-02-08 01:38:41 +0100

    feat: Use stored baby info for weight stats
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>

--- src/features/Statistics/WeightStatistics.tsx
+++ src/features/Statistics/WeightStatistics.tsx
@@ -1,20 +1,20 @@
-import { Text, Title } from '@mantine/core';
+import { Alert, Box, Button, Stack, Text, Title } from '@mantine/core';
 import classes from './Statistics.module.css';
 import { WeightCentileChart } from './WeightCentileChart';
 import { useAppStore } from '../../common/store/store';
 import { EntryType } from '../../common/store/types/storeData.types';
 import dayjs from 'dayjs';
+import { IconInfoCircle } from '@tabler/icons-react';
+import { Link } from 'react-router';
+import { routes } from '../../common/routes';
 
 export const WeightStatistics = () => {
     const entries = useAppStore((state) => state.data.logs);
+    const babyInfo = useAppStore((state) => state.data.babyInfo);
 
     const weightEntries = entries.filter(
         (entry) => entry.entryType === EntryType.WeightMeasurement
     );
-
-    // TODO: Example data, make this configurable
-    const gender = 'MALE';
-    const dateOfBirth = dayjs('2020-01-01T12:00:00Z');
 
     return (
         <>
@@ -28,11 +28,38 @@ export const WeightStatistics = () => {
                     Weight Statistics
                 </Text>
             </Title>
-            <WeightCentileChart
-                entries={weightEntries}
-                gender={gender}
-                dateOfBirth={dateOfBirth}
-            />
+            {babyInfo ? (
+                <WeightCentileChart
+                    entries={weightEntries}
+                    gender={babyInfo.gender}
+                    dateOfBirth={dayjs(babyInfo.dateOfBirth)}
+                />
+            ) : (
+                <Alert
+                    mt="lg"
+                    variant="light"
+                    color="yellow"
+                    title="Missing info"
+                    icon={<IconInfoCircle />}
+                >
+                    <Stack gap={24}>
+                        <Box>
+                            It seems you've not provided your baby's info
+                            required to calculate weight statistics.
+                        </Box>
+                        <Box>
+                            <Button
+                                component={Link}
+                                to={routes.settingsBabyInfo}
+                                variant="filled"
+                                color="primary"
+                            >
+                                Edit info
+                            </Button>
+                        </Box>
+                    </Stack>
+                </Alert>
+            )}
         </>
     );
 };
