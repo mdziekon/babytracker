7698d75215200d5a77a106ba06fbd6a5ac815045
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2025-02-15 01:10:30 +0100

    feat: Display last 5 recent Medicine events
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>

--- src/features/EventEditor/common/RecentEvents/RecentEvents.tsx
+++ src/features/EventEditor/common/RecentEvents/RecentEvents.tsx
@@ -4,20 +4,22 @@ import { Card, Text } from '@mantine/core';
 
 interface RecentEventsProps {
     eventType: EntryType;
+    eventsLimit?: number;
 }
 
 export const RecentEvents = (props: RecentEventsProps) => {
+    const { eventType, eventsLimit = 3 } = props;
+
     return (
         <Card withBorder padding="lg" radius="md">
             <Card.Section withBorder inheritPadding py="xs">
                 <Text fw={700}>Recent events</Text>
             </Card.Section>
             <Card.Section px="md">
-                {/* TODO: Fix th rendering on desktop */}
                 <LogsTable
                     filters={{
-                        limit: 3,
-                        eventType: [props.eventType.replace('EntryType.', '')],
+                        limit: eventsLimit,
+                        eventType: [eventType.replace('EntryType.', '')],
                     }}
                 />
             </Card.Section>
